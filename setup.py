bd55c9586462a9786ac395e0e5d76b144c019d29
Koushik <koushik20040804@gmail.com>
2024-09-09 11:06:57 +0530

    fixed requirements.txt

--- setup.py
+++ setup.py
@@ -1,31 +1,38 @@
 from setuptools import setup, find_packages
 
-with open("README.md") as readme_file:
+with open("README.md", encoding="utf-8") as readme_file:
     README = readme_file.read()
 
-setup_args = dict(
+setup(
     name="voxws",
     version="1.0.1",
     description="Few Shot Language Agnostic Keyword Spotting (FSLAKWS) System",
-    long_description_content_type="text/markdown",
     long_description=README,
-    license="Apache-2.0",
-    packages=['voxws'],
+    long_description_content_type="text/markdown",
     author="Koushik S",
     author_email="koushik20040804@gmail.com",
-    keywords=["Keyword Spotting", "Few-shot Learning", "Deep Neural Network", "Audio", "Speech"],
     url="https://github.com/FewshotML/plix",
-    download_url="https://pypi.org/project/voxws/",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "torch",
+        "torchvision",
+        "torchaudio",
+        "timm",
+        "wget",
+        "librosa"
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+    ],
+    python_requires='>=3.8',
+    license="Apache-2.0",
+    keywords=["Keyword Spotting", "Few-shot Learning", "Deep Neural Network", "Audio", "Speech"],
 )
-
-install_requires = [
-    "torch",
-    "torchvision",
-    "torchaudio",
-    "timm",
-    "wget",
-    "librosa"
-]
-
-if __name__ == "__main__":
-    setup(**setup_args, install_requires=install_requires)
